642854e5228931090848cfaf7e73dc5d62b7a3d7
Michael Salaverry <michael@quris.ai>
2022-12-26 15:10:24 +0200

    feat: get average temperatures for all cities in forecast list

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
+use serde::{Deserialize, Serialize};
+use serde_xml_rs::{from_str};
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "PascalCase")]
+struct LocationForecasts {
+    location: Vec<Location>
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "PascalCase")]
+struct Location {
+    location_meta_data: LocationMetaData,
+    location_data: LocationData
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "PascalCase")]
+struct LocationMetaData {
+    location_id: i16,
+    location_name_eng: String,
+    display_lat: f32,
+    display_lon: f32,
+    display_height: i16,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "PascalCase")]
+struct LocationData {
+    forecast: Vec<Forecast>
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "PascalCase")]
+struct Forecast {
+    forecast_time: String,
+    temperature: i16,
+    relative_humidity: i16,
+    wind_speed: i16,
+    rain: f32,
+    wind_direction: i16,
+    dew_point_temp: i16,
+    heat_stress: i16,
+    heat_stress_level: i16,
+    feels_like: i16,
+    wind_chill: i16,
+    weather_code: i32,
+    min_temp: i16,
+    max_temp: i16,
+    uv_index: Option<i16>,
+    uv_index_max: Option<i16>,
+}
+
 fn main() {
     let weather_url = "https://ims.gov.il/sites/default/files/ims_data/xml_files/isr_cities_1week_6hr_forecast.xml";
     let forecast_xml = ureq::get(weather_url)
@@ -6,25 +59,15 @@ fn main() {
         .into_string()
         .expect("invalid xml");
 
-    let forecast = roxmltree::Document::parse(&forecast_xml).expect("failed to parse xml");
+    let forecasts: LocationForecasts = from_str(&forecast_xml).expect("failed to parse xml");
 
-    let tlv = forecast
-        .descendants()
-        .find(|node| {
-            return node
-                .children()
-                .find(|node| node.tag_name().name() == "LocationId" && node.text() == Some("2"))
-                .is_some();
-        })
-        .expect("failed to find TLV LocationMetaData")
-        .parent()
-        .expect("failed to find TLV parent");
+    forecasts.location.iter().for_each(|location| {
+        print_location(location);
+    });
+}
 
-    let tlv_forecasts = tlv
-        .children()
-        .filter(|node| node.tag_name().name() == "LocationData")
-        .collect::<Vec<roxmltree::Node>>();
-
-    // let avg_temp = temps.fold((0.0, 0), |(sum, count), temp| (sum + temp, count + 1));
-    println!("{:?}", tlv_forecasts);
+fn print_location(location: &Location) {
+    println!("");
+    println!("Location: {}", location.location_meta_data.location_name_eng);
+    println!("Average Temperature: {}", location.location_data.forecast.iter().map(|f| f.temperature).sum::<i16>() / location.location_data.forecast.len() as i16);
 }
