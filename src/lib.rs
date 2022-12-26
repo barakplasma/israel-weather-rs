7399ec728495ef47c1f17f8532ca76e78f41ab72
Michael Salaverry <michael@quris.ai>
2022-12-26 15:31:09 +0200

    refactor: lib from main

---
+++ src/lib.rs
@@ -0,0 +1,66 @@
+use serde::{Deserialize, Serialize};
+use serde_xml_rs::{from_str};
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "PascalCase")]
+pub struct LocationForecasts {
+    pub location: Vec<Location>
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "PascalCase")]
+pub struct Location {
+    pub location_meta_data: LocationMetaData,
+    pub location_data: LocationData
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "PascalCase")]
+pub struct LocationMetaData {
+    pub location_id: i16,
+    pub location_name_eng: String,
+    pub display_lat: f32,
+    pub display_lon: f32,
+    pub display_height: i16,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "PascalCase")]
+pub struct LocationData {
+    pub forecast: Vec<Forecast>
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "PascalCase")]
+pub struct Forecast {
+    pub forecast_time: String,
+    pub temperature: i16,
+    pub relative_humidity: i16,
+    pub wind_speed: i16,
+    pub rain: f32,
+    pub wind_direction: i16,
+    pub dew_point_temp: i16,
+    pub heat_stress: i16,
+    pub heat_stress_level: i16,
+    pub feels_like: i16,
+    pub wind_chill: i16,
+    pub weather_code: i32,
+    pub min_temp: i16,
+    pub max_temp: i16,
+    pub uv_index: Option<i16>,
+    pub uv_index_max: Option<i16>,
+}
+
+static WEATHER_URL: &str = "https://ims.gov.il/sites/default/files/ims_data/xml_files/isr_cities_1week_6hr_forecast.xml";
+
+pub fn get_israeli_weather_forecast() -> Result<LocationForecasts, serde_xml_rs::Error> {
+    let forecast_xml = ureq::get(WEATHER_URL)
+        .call()
+        .expect("failed to fetch forecast")
+        .into_string()
+        .expect("invalid xml");
+
+    let forecasts: Result<LocationForecasts, serde_xml_rs::Error> = from_str(&forecast_xml);
+
+    return forecasts;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,14 @@
-use serde::{Deserialize, Serialize};
-use serde_xml_rs::{from_str};
-
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
-#[serde(rename_all = "PascalCase")]
-struct LocationForecasts {
-    location: Vec<Location>
-}
-
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
-#[serde(rename_all = "PascalCase")]
-struct Location {
-    location_meta_data: LocationMetaData,
-    location_data: LocationData
-}
-
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
-#[serde(rename_all = "PascalCase")]
-struct LocationMetaData {
-    location_id: i16,
-    location_name_eng: String,
-    display_lat: f32,
-    display_lon: f32,
-    display_height: i16,
-}
-
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
-#[serde(rename_all = "PascalCase")]
-struct LocationData {
-    forecast: Vec<Forecast>
-}
-
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
-#[serde(rename_all = "PascalCase")]
-struct Forecast {
-    forecast_time: String,
-    temperature: i16,
-    relative_humidity: i16,
-    wind_speed: i16,
-    rain: f32,
-    wind_direction: i16,
-    dew_point_temp: i16,
-    heat_stress: i16,
-    heat_stress_level: i16,
-    feels_like: i16,
-    wind_chill: i16,
-    weather_code: i32,
-    min_temp: i16,
-    max_temp: i16,
-    uv_index: Option<i16>,
-    uv_index_max: Option<i16>,
-}
+use weather::get_israeli_weather_forecast;
 
 fn main() {
-    let weather_url = "https://ims.gov.il/sites/default/files/ims_data/xml_files/isr_cities_1week_6hr_forecast.xml";
-    let forecast_xml = ureq::get(weather_url)
-        .call()
-        .expect("failed to fetch forecast")
-        .into_string()
-        .expect("invalid xml");
-
-    let forecasts: LocationForecasts = from_str(&forecast_xml).expect("failed to parse xml");
+    let forecasts = get_israeli_weather_forecast().expect("failed to get forecast");
 
     forecasts.location.iter().for_each(|location| {
         print_location(location);
     });
 }
 
-fn print_location(location: &Location) {
+fn print_location(location: &weather::Location) {
     println!("");
     println!("Location: {}", location.location_meta_data.location_name_eng);
     println!("Average Temperature: {}", location.location_data.forecast.iter().map(|f| f.temperature).sum::<i16>() / location.location_data.forecast.len() as i16);
