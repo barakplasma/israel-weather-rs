eb971a2e6cd45a5d4d7a4dea6b336a2fb8db9982
Michael Salaverry <michael@quris.ai>
2023-02-01 10:36:11 +0200

    feat: add cache #4 https://github.com/barakplasma/israel-weather-rs/issues/4

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,5 @@ use weather::get_israeli_weather_forecast;
 fn main() {
     let forecasts = get_israeli_weather_forecast().expect("failed to get forecast");
 
-    forecasts.location.iter().for_each(|location| {
-        print_location(location);
-    });
-}
-
-fn print_location(location: &weather::Location) {
-    println!("");
-    println!("Location: {}", location.location_meta_data.location_name_eng);
-    println!("Most Recent Forecast:");
-    let most_recent_forecast = location.location_data.forecast.last().expect("failed to get most recent forecast");
-    if Some(most_recent_forecast) != None {
-        println!("Time: {}", most_recent_forecast.forecast_time);
-        println!("Temperature: {}", most_recent_forecast.temperature);
-        println!("Wind Direction: {}", most_recent_forecast.wind_direction);
-        println!("Wind Speed: {}", most_recent_forecast.wind_speed);
-        println!("Rain: {}", most_recent_forecast.rain);
-        println!("");
-    }
+    println!("{:#?}", forecasts.location)
 }
