6e834610793f2a2f272ee80087733e187c1bf22a
Michael Salaverry <michael@quris.ai>
2023-02-09 09:45:23 +0200

    feat: translate weather code to english

--- src/lib.rs
+++ src/lib.rs
@@ -46,6 +46,7 @@ pub struct Forecast {
     pub feels_like: i16,
     pub wind_chill: i16,
     pub weather_code: i32,
+    pub weather_code_english: Option<String>,
     pub min_temp: i16,
     pub max_temp: i16,
     pub uv_index: Option<i16>,
@@ -67,6 +68,7 @@ pub fn get_israeli_weather_forecast() -> Result<LocationForecasts, i8> {
 
     if let Ok(mut forecasts) = forecasts {
         transform_forecast_times_to_datetimes(&mut forecasts);
+        transform_weather_code_to_english(&mut forecasts);
         return Ok(forecasts);
     } else {
         return Err(0);
@@ -86,6 +88,39 @@ fn transform_forecast_times_to_datetimes(forecast: &mut LocationForecasts) {
     forecast.location.iter_mut().for_each(|location| {
         location.location_data.forecast.iter_mut().for_each(|forecast| {
             forecast.forecast_time = parse_time(&forecast.forecast_time).expect("failed to parse forecast time").to_rfc3339();
+        });
+    });
+}
+
+fn transform_weather_code_to_english(forecast: &mut LocationForecasts) {
+    forecast.location.iter_mut().for_each(|location| {
+        location.location_data.forecast.iter_mut().for_each(|forecast| {
+            forecast.weather_code_english = Some(match forecast.weather_code {
+                1010 => "Sandstorms",
+                1020 => "Thunderstorms",
+                1060 => "Snow",
+                1070 => "Light snow",
+                1080 => "Sleet",
+                1140 => "Rainy",
+                1160 => "Fog",
+                1220 => "Partly cloudy",
+                1230 => "Cloudy",
+                1250 => "Clear",
+                1260 => "Windy",
+                1270 => "Muggy",
+                1300 => "Frost",
+                1310 => "Hot",
+                1320 => "Cold",
+                1510 => "Stormy",
+                1520 => "Heavy snow",
+                1530 => "Partly cloudy possible rain",
+                1540 => "Cloudy, possible rain",
+                1560 => "Cloudy, light rain",
+                1570 => "Dust",
+                1580 => "Extremely hot",
+                1590 => "Extremely cold",
+                _ => "Unknown"
+            }).to_owned().map(|s| s.to_string());
         });
     });
 }
