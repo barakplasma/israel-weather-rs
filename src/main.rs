69c34a61b8c67cc19bfff20f7aa0d58b68d8b9b2
Michael Salaverry <michael@quris.ai>
2023-02-09 10:00:41 +0200

    feat: output  pretty printed json

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
+use serde_json;
 use weather::get_israeli_weather_forecast;
 
 fn main() {
     let forecasts = get_israeli_weather_forecast().expect("failed to get forecast");
 
-    println!("{:#?}", forecasts.location)
+    let json = serde_json::json!(forecasts);
+    // pretty print as json
+    println!(
+        "{}",
+        serde_json::to_string_pretty(&json).expect("failed to serialize weather forecast to json")
+    );
 }
