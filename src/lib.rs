2b87835a56ee92797179b7fc5bc22db444f3c63c
Michael Salaverry <barakplasma@gmail.com>
2024-12-29 08:10:06 +0000

    fix: failed to get when internet disconnected feat: logging refactor: update deps and tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+use std::path::PathBuf;
+use log::{trace, warn};
+
 use cached_path::Cache;
 use chrono::{DateTime, LocalResult, NaiveDateTime, Utc};
 use serde_xml_rs::from_str;
@@ -7,7 +10,8 @@ mod ims_structs;
 static WEATHER_URL: &str =
     "https://ims.gov.il/sites/default/files/ims_data/xml_files/isr_cities_1week_6hr_forecast.xml";
 
-pub fn get_israeli_weather_forecast(offline: bool) -> Result<ims_structs::LocationForecasts, serde_xml_rs::Error> {
+fn make_cache(offline: bool) -> PathBuf {
+    trace!("build cache {}", offline);
     let cache = Cache::builder()
         .dir(std::env::temp_dir().join("weather/"))
         .connect_timeout(std::time::Duration::from_secs(60))
@@ -16,15 +20,28 @@ pub fn get_israeli_weather_forecast(offline: bool) -> Result<ims_structs::Locati
         .build()
         .expect("unable to start download cache");
 
-    let path = cache
-        .cached_path(WEATHER_URL)
-        .expect("failed to download forecast xml");
+    let result = cache.cached_path(WEATHER_URL);
 
-    assert!(path.is_file());
+    if result.is_err() {
+        trace!("cache error");
+        warn!("{}", result.unwrap_err());
+        return make_cache(true);
+    }
 
-    let forecast_xml = std::fs::read_to_string(path).expect("failed to read forecast xml");
+    return result.expect("cache creation failed");
+}
 
-    let forecasts: Result<ims_structs::LocationForecasts, serde_xml_rs::Error> = from_str(&forecast_xml);
+pub fn get_israeli_weather_forecast(
+    offline: bool,
+) -> Result<ims_structs::LocationForecasts, serde_xml_rs::Error> {
+    let xml_path = make_cache(offline);
+
+    trace!("{}", xml_path.display());
+
+    let forecast_xml = std::fs::read_to_string(xml_path).expect("failed to read forecast xml");
+
+    let forecasts: Result<ims_structs::LocationForecasts, serde_xml_rs::Error> =
+        from_str(&forecast_xml);
 
     if let Ok(mut forecasts) = forecasts {
         transform_forecast_times_to_datetimes(&mut forecasts);
@@ -130,19 +147,22 @@ mod tests {
 
     #[test]
     fn transform_weather_code() {
-        let xml = std::fs::read_to_string("./weather.xml").unwrap();
+        let xml = std::fs::read_to_string("./isr_cities_1week_6hr_forecast.xml").unwrap();
         let mut forecasts: ims_structs::LocationForecasts = serde_xml_rs::from_str(&xml).unwrap();
         super::transform_weather_code_to_english(&mut forecasts);
-        let english = forecasts.location[0].location_data.forecast[0].weather_code_english.as_ref().unwrap();
-        assert_eq!(english, "Clear");
+        let english = forecasts.location[0].location_data.forecast[0]
+            .weather_code_english
+            .as_ref()
+            .unwrap();
+        assert_eq!(english, "Fog");
     }
 
     #[test]
     fn transform_forecast_times_to_datetimes() {
-        let xml = std::fs::read_to_string("./weather.xml").unwrap();
+        let xml = std::fs::read_to_string("./isr_cities_1week_6hr_forecast.xml").unwrap();
         let mut forecasts: ims_structs::LocationForecasts = serde_xml_rs::from_str(&xml).unwrap();
         super::transform_forecast_times_to_datetimes(&mut forecasts);
         let time = &forecasts.location[0].location_data.forecast[0].forecast_time;
-        assert_eq!(time, "2023-06-25T03:00:00+00:00");
+        assert_eq!(time, "2024-12-28T02:00:00+00:00");
     }
 }
